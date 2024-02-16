34d0a682ad005614614eb9e0d3c158a83d32dbe9
Aliaksei <shebalinas2000@gmail.com>
2024-02-16 08:29:20 +0100

    feat: encode-line is done

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given a string, return its encoding version.
@@ -11,10 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(str) {
-
+    let count = 1;
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count += 1;
+        } else if (count > 1) {
+            arr.push(count, str[i])
+            count = 1
+        } else {
+            arr.push(str[i]);
+        }
+    }
+    return arr.join('')
 }
 
 console.log(encodeLine('aabbbc'))
 module.exports = {
-  encodeLine
+    encodeLine
 };
