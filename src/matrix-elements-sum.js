1f270621a6b514b0f0e646aebfc0955fb730d846
Aliaksei <shebalinas2000@gmail.com>
2024-02-17 12:44:58 +0100

    feat: get-email-domain.js,hanoi-tower.js,mac-address.js,matrix-elements-sum.js are done

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+    let sum =0;
+    let bellow = Array(matrix[0].length).fill(false)
+    for (let i = 0; i <matrix.length; i++){
+        for (let j = 0; j < bellow.length; j ++){
+            if (matrix[i][j] === 0){
+                bellow[j] = true
+            }else if(!bellow[j]){
+                sum += matrix[i][j]
+            }
+        }
+    }
+    return sum
 }
 
+console.log(getMatrixElementsSum([
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3],
+]))
+
 module.exports = {
-  getMatrixElementsSum
+    getMatrixElementsSum
 };
